7ca093768d96af7ad3d9e91f257573c5fb69eff8
vlaaad <v.proczenko@gmail.com>
2017-03-14 11:41:38 +0300

    support scalar type generation

--- src/fhir_2_gql/core.clj
+++ src/fhir_2_gql/core.clj
@@ -16,23 +16,23 @@
 (defn- fhir->gql-type-name [type-info]
   (let [name (:code type-info)]
     (condp #(%1 %2) name
-      #{"dateTime"
-        "instant"}       "Instant" ;; TODO define Instant scalar type in overall gql schema
-      #{"time"}          "Time"    ;; TODO define Time scalar type in overall gql schema
-      #{"date"}          "Date"    ;; TODO define Date scalar type in overall gql schema
       #{"boolean"}       "Boolean"
       #{"id"}            "ID"
       #{"decimal"}       "Float"
       #{"integer"
         "positiveInt"
         "unsignedInt"}   "Int"
-      #{"string"
-        "code"
-        "base64Binary"
-        "markdown"
-        "xhtml"
+      #{"oid"}           "Uri"
+      #{"time"
+        "date"
+        "dateTime"
+        "instant"
+        "string"
         "uri"
-        "oid"}           "String"
+        "base64Binary"}  (capitalize-first name)
+      #{"code"
+        "markdown"
+        "xhtml"}           "String"
       #{"Reference"}     (str/replace (first (:profile type-info)) #"^.+/" "")
       first-capitalized? name)))
 
@@ -41,34 +41,48 @@
 
 ;; TODO union types: detect when there are many field types and extract
 ;; TODO enums from codes
-;; TODO do not fail on scalars
+;; TODO abstract types
 
 (defn- add-field-info [acc type-name field-info]
   (update-in acc [type-name :fields] (fn [coll]
                                        {:pre [(not (nil? coll))]}
                                        (conj coll field-info))))
 
+(defn- path->type-name
+  [path]
+  (str/join (map capitalize-first path)))
+
 (defn- override-type-name [type-name acc path]
   (if (#{"BackboneElement" "Element"} type-name)
-    (let [type-name (str/join (map capitalize-first path))]
+    (let [type-name (path->type-name path)]
       [(add-type-declaration acc type-name) type-name])
     [acc type-name]))
 
+(defn- path
+  [element]
+  (str/split (:path element) #"\."))
+
+(defn- scalar? [element]
+  (and (:_code (first (:type element)))
+       (= "value" (last (path element)))))
+
+(defn- field? [element]
+  (and (not (:sliceName element)) (:type element)))
+
 (defn- process-element [acc element]
-  (let [path (str/split (:path element) #"\.")]
+  (let [path (path element)]
     (if (= 1 (count path))
       (add-type-declaration acc (capitalize-first (first path)))
-      (let [entity-type (str/join (map capitalize-first (butlast path)))
-            field       (str/replace (last path) #"\[x\]$" "")
-            cardinality (cardinality element)]
-        (if (and (not (:sliceName element))
-                 (:type element))
-          (let [type-name (fhir->gql-type-name (first (:type element)))
-                [acc type-name]       (override-type-name type-name acc path)]
-            (add-field-info acc entity-type {:field       field
-                                             :cardinality cardinality
+      (cond
+        (scalar? element) (reduced {(path->type-name (butlast path)) {:kind :scalar}})
+        (field? element)  (let [type-name       (fhir->gql-type-name (first (:type element)))
+                                [acc type-name] (override-type-name type-name acc path)]
+                            (add-field-info acc
+                                            (path->type-name (butlast path))
+                                            {:field       (str/replace (last path) #"\[x\]$" "")
+                                             :cardinality (cardinality element)
                                              :type        type-name}))
-          acc)))))
+        :else             acc))))
 
 ;; rendering
 
@@ -94,7 +108,8 @@
                                   (map
                                    (fn [{:keys [field cardinality type]}]
                                      (str field ": " (cardinality->gql cardinality type))))))
-                   "\n}")))
+                   "\n}")
+        :scalar (str "scalar " type-name)))
     coll)))
 
 (defn structure-def->gql-type-map [json-str]
